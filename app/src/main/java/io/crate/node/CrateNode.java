78b4b403427710217a091d5c12c3db693464b4f8
Marios Trivyzas <marios@crate.io>
2017-01-18 13:07:04 +0200

    WIP: discovery-Ec2 isolated classloaded

--- app/src/main/java/io/crate/node/CrateNode.java
+++ app/src/main/java/io/crate/node/CrateNode.java
@@ -23,22 +23,16 @@
 package io.crate.node;
 
 import com.google.common.collect.ImmutableList;
-import io.crate.Build;
-import io.crate.Version;
-import io.crate.plugin.*;
+import io.crate.plugin.BlobPlugin;
+import io.crate.plugin.CrateCorePlugin;
+import io.crate.plugin.PluginLoaderPlugin;
+import io.crate.plugin.SrvPlugin;
 import io.crate.udc.plugin.UDCPlugin;
-import org.apache.logging.log4j.Logger;
-import org.apache.lucene.util.Constants;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
-import org.elasticsearch.monitor.jvm.JvmInfo;
 import org.elasticsearch.node.Node;
-import org.elasticsearch.plugin.discovery.ec2.Ec2DiscoveryPlugin;
-import org.elasticsearch.plugin.repository.s3.S3RepositoryPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.transport.Netty3Plugin;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 public class CrateNode extends Node {
@@ -51,36 +45,12 @@ public class CrateNode extends Node {
         //MulticastDiscoveryPlugin.class,
         SrvPlugin.class,
         UDCPlugin.class,
-        S3RepositoryPlugin.class,
-        Ec2DiscoveryPlugin.class,
-        //AdminUIPlugin.class,
+//        S3RepositoryPlugin.class,
+//        Ec2DiscoveryPlugin.class,
+//        AdminUIPlugin.class,
         Netty3Plugin.class);
 
     protected CrateNode(Environment environment) {
         super(environment, CLASSPATH_PLUGINS);
-    }
-
-    @Override
-    protected void startUpLogging(Logger logger, Settings tmpSettings, boolean hadPredefinedNodeName) {
-        final JvmInfo jvmInfo = JvmInfo.jvmInfo();
-        logger.info(
-            "CrateDB version[{}], pid[{}], build[{}/{}], OS[{}/{}/{}], JVM[{}/{}/{}/{}]",
-            Version.CURRENT,
-            jvmInfo.pid(),
-            Build.CURRENT.hashShort(),
-            Build.CURRENT.timestamp(),
-            Constants.OS_NAME,
-            Constants.OS_VERSION,
-            Constants.OS_ARCH,
-            Constants.JVM_VENDOR,
-            Constants.JVM_NAME,
-            Constants.JAVA_VERSION,
-            Constants.JVM_VERSION);
-
-        if (logger.isDebugEnabled()) {
-            Environment environment = getEnvironment();
-            logger.debug("using config [{}], data [{}], logs [{}], plugins [{}]",
-                environment.configFile(), Arrays.toString(environment.dataFiles()), environment.logsFile(), environment.pluginsFile());
-        }
     }
 }
