f1da861b33836d47089420a7039c5ad7b128a4d8
Andrei Dan <andrei.dan@crate.io>
2017-04-18 23:31:51 +0100

    Upgrade crate version to point to ES 5.3

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -41,7 +41,8 @@ public class Version {
 
 
     public static final boolean SNAPSHOT = true;
-    public static final Version CURRENT = new Version(1020099, SNAPSHOT, org.elasticsearch.Version.V_5_0_2);
+    // FIXME whys is ES version v5.3.0 still UNRELEASED ?
+    public static final Version CURRENT = new Version(1020099, SNAPSHOT, org.elasticsearch.Version.V_5_3_0_UNRELEASED);
 
     static {
         // safe-guard that we don't release a version with DEBUG_MODE set to true
